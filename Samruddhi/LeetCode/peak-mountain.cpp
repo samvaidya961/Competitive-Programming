b92daa1d11e7d36734bbbd4dddccfc2b12b032cb
samvaidya961 <67541221+samvaidya961@users.noreply.github.com>
2022-07-10 14:12:01 +0530

    852. Peak Index in a Mountain Array

---
+++ Samruddhi/LeetCode/peak-mountain.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int peakIndexInMountainArray(vector<int> &arr)
+{
+    int s = 0;
+    int e = arr.size() - 1;
+
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+        if (arr[mid] < arr[mid + 1])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+
+    return s;
+}
+
+int main()
+{
+    vector<int> arr;
+    arr = {2, 3, 6, 7, 0};
+    cout << peakIndexInMountainArray(arr);
+    return 0;
+}
